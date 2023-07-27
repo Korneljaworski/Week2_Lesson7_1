c546225877d62d5b14b24475707ec1da73cff3df
Korjaw Korjaw <kornel.jaworski@gmail.com>
2023-07-28 01:11:31 +0200

    Add project files.

---
+++ Week2_Lesson7_1/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+            Napisz program w C#, który stworzy dwie zmienne int i sprawdzi czy są one równe czy nie.
+            Dane testowe:
+            a : 5
+            b : 5
+            Rezultat w terminalu : 5 i 5 są równe
+        */
+
+        int a = 5, b = 5;
+
+        if (a == b)
+        {
+            Console.WriteLine($"{a} i {b} są równe");
+        }
+        else
+        {
+            Console.WriteLine($"{a} i {b} nie są równe");
+        }
+    }
+
+}
+
